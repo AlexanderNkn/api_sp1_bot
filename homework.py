c84849bd14aa187a7248ca7f2d32e5f0c0449a10
Alexander <spresi@mail.ru>
2020-03-28 19:31:26 +0300

    added exceptions to request to yandex

--- homework.py
+++ homework.py
@@ -1,3 +1,4 @@
+import json
 import os
 import time
 
@@ -7,15 +8,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-proxy_url = os.getenv('proxy_url')
+PROXY_URL = os.getenv('proxy_url')
 
-proxy = telegram.utils.request.Request(proxy_url=proxy_url)
+proxy = telegram.utils.request.Request(proxy_url=PROXY_URL)
 bot = telegram.Bot(token=TELEGRAM_TOKEN, request=proxy)
-
 
 def parse_homework_status(homework):
     homework_name = homework.get('homework_name')
@@ -29,12 +28,22 @@ def parse_homework_status(homework):
 def get_homework_statuses(current_timestamp):
     headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
     params = {'from_date': current_timestamp}
-    homework_statuses = requests.get(
-        'https://praktikum.yandex.ru/api/user_api/homework_statuses/',
-        headers=headers, 
-        params=params
-    )
-    return homework_statuses.json()
+    while True:
+        try:
+            homework_statuses = requests.get(
+                'https://praktikum.yandex.ru/api/user_api/homework_statuses/',
+                headers=headers, 
+                params=params
+            )
+            homework_statuses.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            print(f'Сервер яндекса упал с ошибкой: {e}')
+        else:
+            try:
+                return homework_statuses.json()
+            except json.JSONDecodeError:
+                print(f'Сервер отвечает, но в возвращаемой с сайта странице нет json')
+        time.sleep(5)
 
 
 def send_message(message):
@@ -42,18 +51,18 @@ def send_message(message):
 
 
 def main():
-    current_timestamp = int(time.time())  # начальное значение timestamp
+    current_timestamp = 0  # начальное значение timestamp
     
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
-            if new_homework.get('homeworks'):
+            if new_homework.get('homeworks', [],):
                 send_message(parse_homework_status(new_homework.get('homeworks')[0]))
             current_timestamp = new_homework.get('current_date')  # обновить timestamp
             time.sleep(1200)  # опрашивать раз в 20 минут
 
         except Exception as e:
-            print(f'Бот упал с ошибкой: {e}')
+            print(f'Бот упал с ошибкой: {e}; имя ошибки: {e.__class__.__name__}')
             time.sleep(5)
 
 
